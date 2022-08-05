b7d4054d125bd4f329e02b0527cee087d5d8352a
Natalia Hernandez <natalia.hernandez@clarity.ai>
2022-08-05 17:43:46 +0200

    kata MeanOfTwoMeans test if nfirst and nlast is less than 1 and greater than array's length.

--- src/main/java/mean/two/means/MeanOfTwoMeans.java
+++ src/main/java/mean/two/means/MeanOfTwoMeans.java
@@ -3,10 +3,31 @@ package mean.two.means;
 import java.util.Arrays;
 
 public class MeanOfTwoMeans {
-    public double getMean(int[] arrayNumber, Integer nFirstNumbers) {
-    int[] nFirstNumbersArray = Arrays.copyOfRange(arrayNumber,0,nFirstNumbers);
+    public double getMean(int[] arrayNumber, Integer nFirstNumbers, Integer nLastNumbers) {
+        int[] nFirstNumbersArray = Arrays.copyOfRange(arrayNumber, 0, nFirstNumbers);
+        int[] nLastNumbersArray = Arrays.copyOfRange(arrayNumber, arrayNumber.length - nLastNumbers, arrayNumber.length);
 
-    return Arrays.stream(nFirstNumbersArray).mapToDouble(number -> number).sum() / nFirstNumbersArray.length;
+        if (nFirstNumbers > 1 && nLastNumbers > 1 && nFirstNumbers <= arrayNumber.length && nLastNumbers <= arrayNumber.length) {
+            if (nFirstNumbers != null && nLastNumbers != null) {
+                double nFirstElementsMean = Arrays.stream(nFirstNumbersArray).mapToDouble(number -> number).sum() / nFirstNumbersArray.length;
+                double nLastElementsMean = Arrays.stream(nLastNumbersArray).mapToDouble(number -> number).sum() / nLastNumbersArray.length;
+                return (nFirstElementsMean + nLastElementsMean) / 2;
+            }
+
+            if (nFirstNumbers != null) {
+
+                return Arrays.stream(nFirstNumbersArray).mapToDouble(number -> number).sum() / nFirstNumbersArray.length;
+            }
+
+            if (nLastNumbers != null) {
+                return Arrays.stream(nLastNumbersArray).mapToDouble(number -> number).sum() / nLastNumbersArray.length;
+            }
+        }
+
+
+        return -1;
+
+
     }
 
 
--- src/test/java/mean/two/means/MeanOfTwoMeansTest.java
+++ src/test/java/mean/two/means/MeanOfTwoMeansTest.java
@@ -5,14 +5,59 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MeanOfTwoMeansTest {
+
     @Test
-    void mean_of_n_first_numbers_of_the_array(){
+    void mean_of_n_first_numbers_of_the_array() {
         double expectedResult = 2;
         Integer nFirstNumbers = 2;
-        int [] arrayNumber = new int[] {1,3,2,4};
+        Integer nLastNumbers = null;
+        int[] arrayNumber = new int[]{1, 3, 2, 4};
         MeanOfTwoMeans meanOfTwoMeans = new MeanOfTwoMeans();
 
-        double meanResult = meanOfTwoMeans.getMean(arrayNumber, nFirstNumbers);
+        double meanResult = meanOfTwoMeans.getMean(arrayNumber, nFirstNumbers, nLastNumbers);
 
-        assertThat(meanResult).isEqualTo(expectedResult);}
+        assertThat(meanResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void mean_of_n_last_numbers_of_the_array() {
+        double expectedResult = 3;
+        Integer nLastNumbers = 3;
+        Integer nFirstNumbers = null;
+        int[] arrayNumber = new int[]{1, 3, 2, 4};
+
+        MeanOfTwoMeans meanOfTwoMeans = new MeanOfTwoMeans();
+
+        double meanResult = meanOfTwoMeans.getMean(arrayNumber, nFirstNumbers, nLastNumbers);
+
+        assertThat(meanResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void mean_of_n_first_numbers_mean_and_n_first_numbers_mean(){
+        double expectedResult = 2.5;
+        Integer nLastNumbers = 3;
+        Integer nFirstNumbers = 2;
+        int[] arrayNumber = new int[]{1, 3, 2, 4};
+
+        MeanOfTwoMeans meanOfTwoMeans = new MeanOfTwoMeans();
+
+        double meanResult = meanOfTwoMeans.getMean(arrayNumber, nFirstNumbers, nLastNumbers);
+
+        assertThat(meanResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void if_n_first_numbers_and_n_last_numbers_are_less_than_1_and_greater_than_arrays_length_returns_negative1(){
+        double expectedResult = -1.;
+        Integer nLastNumbers = 4;
+        Integer nFirstNumbers = 0;
+        int[] arrayNumber = new int[]{1, 3, 2, 4};
+
+        MeanOfTwoMeans meanOfTwoMeans = new MeanOfTwoMeans();
+
+        double meanResult = meanOfTwoMeans.getMean(arrayNumber, nFirstNumbers, nLastNumbers);
+
+        assertThat(meanResult).isEqualTo(expectedResult);
+    }
 }
