fb9e26cef23c164fc9fda8cdbdf95075a5c28156
Natalia H <nholea@gmail.com>
2022-08-07 12:47:13 +0200

    Add card suit kata

--- src/main/java/card/suit/CardSuit.java
+++ src/main/java/card/suit/CardSuit.java
@@ -4,11 +4,11 @@ import java.nio.charset.StandardCharsets;
 
 public class CardSuit {
     public String whatCardSuitIs(String inputString) {
-        if (inputString.substring(1,2).contentEquals("?"))
+        if (inputString.substring(1,2).contentEquals(" ? "))
             return "clubs";
-        if (inputString.substring(1,2).contentEquals("?"))
+        if (inputString.substring(1,2).contentEquals(" ? "))
             return "diamonds";
-        if (inputString.substring(1,2).contentEquals("?"))
+        if (inputString.substring(1,2).contentEquals(" ? "))
             return "hearts";
 
         return "spades";
