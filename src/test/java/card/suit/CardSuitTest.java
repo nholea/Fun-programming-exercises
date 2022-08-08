664cb2d704dfc8c280a72f93f4b2ea0f2e948dec
Natalia H <75338615+nholea@users.noreply.github.com>
2022-08-08 09:14:31 +0200

    Update CardSuitTest.java

--- src/test/java/card/suit/CardSuitTest.java
+++ src/test/java/card/suit/CardSuitTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CardSuitTest {
     @Test
     void suit_of_card_is_clubs(){
-        String inputCard = "3?";
+        String inputCard = "3♣";
         String expectedSuit = "clubs";
         CardSuit cardsuit = new CardSuit();
 
@@ -19,7 +19,7 @@ public class CardSuitTest {
 
     @Test
     void suit_of_card_is_diamonds(){
-        String inputCard = "3?";
+        String inputCard = "3♦";
         String expectedSuit = "diamonds";
         CardSuit cardsuit = new CardSuit();
 
@@ -28,7 +28,7 @@ public class CardSuitTest {
     }
     @Test
     void suit_of_card_is_hearts(){
-        String inputCard = "3?";
+        String inputCard = "3♥";
         String expectedSuit = "hearts";
         CardSuit cardsuit = new CardSuit();
 
@@ -38,7 +38,7 @@ public class CardSuitTest {
 
     @Test
     void suit_of_card_is_spades(){
-        String inputCard = "3?";
+        String inputCard = "3♠";
         String expectedSuit = "spades";
         CardSuit cardsuit = new CardSuit();
 
