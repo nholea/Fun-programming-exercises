4e3c7d2c193024a68d053a53a981872c64d28bd8
Natalia Hernandez <natalia.hernandez@clarity.ai>
2022-08-09 16:59:49 +0200

    Test if array contains a negative numbers as its first element Sum Two Arrays kata

--- src/main/java/sum/two/arrays/SumTwoArrays.java
+++ src/main/java/sum/two/arrays/SumTwoArrays.java
@@ -1,14 +1,51 @@
 package sum.two.arrays;
 
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 public class SumTwoArrays {
     public int[] addArrays(int[] inputArrayNumbers1, int[] inputArrayNumbers2) {
 
-        if (inputArrayNumbers1.length != 0){
+        if (inputArrayNumbers1.length != 0 && inputArrayNumbers2.length != 0) {
+            String inputArrayNumbers1ToString = Arrays.stream(inputArrayNumbers1)
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining());
+            String inputArrayNumbers2ToString = Arrays.stream(inputArrayNumbers2)
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining());
+
+            int sumStrings = Integer.parseInt(inputArrayNumbers1ToString) + Integer.parseInt(inputArrayNumbers2ToString);
+
+            String[] sumStringToArray = String.valueOf(sumStrings).split("");
+
+            if (sumStringToArray[0].equals("-")) {
+                sumStringToArray[1] = "-" + sumStringToArray[1];
+                sumStringToArray = ArrayUtils.removeElement(sumStringToArray, "-");
+            }
+
+            int[] resultSumTwoArrays = Arrays.stream(sumStringToArray).
+                    mapToInt(Integer::parseInt).
+                    toArray();
+
+            return resultSumTwoArrays;
+
+        }
+        ;
+
+        if (inputArrayNumbers1.length != 0) {
             return inputArrayNumbers1;
         }
-        if (inputArrayNumbers2.length != 0){
+        if (inputArrayNumbers2.length != 0) {
             return inputArrayNumbers2;
         }
+
         return new int[]{};
     }
+
 }
--- src/test/java/sum/two/arrays/SumTwoArraysTest.java
+++ src/test/java/sum/two/arrays/SumTwoArraysTest.java
@@ -23,12 +23,37 @@ public class SumTwoArraysTest {
         int[] inputArrayNumbers1 = new int[]{3, 2, 9};
         int[] inputArrayNumbers2 = new int[]{};
         int[] expectedResult = new int[]{3, 2, 9};
-
-
         SumTwoArrays sumTwoArrays = new SumTwoArrays();
+
         int[] addArraysResult = sumTwoArrays.addArrays(inputArrayNumbers1, inputArrayNumbers2);
+
         assertThat(addArraysResult).isEqualTo(expectedResult);
     }
 
+    @Test
+    void if_two_arrays_are_not_empty_return_an_array_of_the_sum_of_both_arrays_in_same_format_as_given_arrays(){
+        int[] inputArrayNumbers1 = new int[]{4,7,3};
+        int[] inputArrayNumbers2 =new int[]{1,2,3};
+        int[] expectedResult = new int[]{5,9,6};
+        SumTwoArrays sumTwoArrays = new SumTwoArrays();
+
+        int[] addArraysResult = sumTwoArrays.addArrays(inputArrayNumbers1, inputArrayNumbers2);
+
+        assertThat(addArraysResult).isEqualTo(expectedResult);
+
+    }
+
+    @Test
+    void if_an_array_contains_a_negative_number_as_first_element_the__number_will_be_considered_as_negative_number(){
+        int[] inputArrayNumbers1 = new int[] {-7,2,2,8};
+        int[] inputArrayNumbers2 = new int[] {3,2,6,6};
+        int[] expectedResult = new int[] {-3,9,6,2};
+        SumTwoArrays sumTwoArrays = new SumTwoArrays();
+
+        int[] addArraysResult = sumTwoArrays.addArrays(inputArrayNumbers1, inputArrayNumbers2);
+
+        assertThat(addArraysResult).isEqualTo(expectedResult);
+
+    }
 }
 
