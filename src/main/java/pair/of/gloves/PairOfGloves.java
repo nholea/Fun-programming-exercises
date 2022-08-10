5991147d6105771b36fe7fafb0ecb9002c399f5e
Natalia Hernandez <natalia.hernandez@clarity.ai>
2022-08-10 15:25:02 +0200

    Test if array contains several gloves with the same color Pair of Gloves kata

--- src/main/java/pair/of/gloves/PairOfGloves.java
+++ src/main/java/pair/of/gloves/PairOfGloves.java
@@ -1,11 +1,33 @@
 package pair.of.gloves;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 public class PairOfGloves {
     public int getNumberPairGloves(String[] inputGloves) {
 
-        if(inputGloves.length ==2 && inputGloves[0].equals(inputGloves[1])){
-            return 1;
+        Map<String, Integer> pairGloves = new HashMap<>();
+        for (String gloves: inputGloves){
+            if(pairGloves.containsKey(gloves)){
+                pairGloves.put(gloves, pairGloves.get(gloves) + 1);
+            }else {
+                pairGloves.put(gloves,1);
+            }
         }
-        return 0;
+        int pairGlovesCount = pairGloves.values().stream().mapToInt(Integer::intValue).sum();
+
+        return pairGlovesCount /2;
+    }
+
+    public static void main(String[] args){
+        Map<String, Integer> pairGloves = new HashMap<>();
+        pairGloves.put("red",5);
+
+        int pairGlovesCount = pairGloves.values().stream().mapToInt(Integer::intValue).sum();
+
+
+        System.out.println("pairGlovesCount: " + pairGlovesCount/2);
+
     }
 }
--- src/test/java/pair/of/gloves/PairOfGlovesTest.java
+++ src/test/java/pair/of/gloves/PairOfGlovesTest.java
@@ -39,4 +39,18 @@ public class PairOfGlovesTest {
 
         assertThat(numberPairGloves).isEqualTo(expectedNumberPairGloves);
     }
+
+
+    @Test
+    void if_the_array_contains_five_gloves_with_the_same_color_return_2(){
+        String[] inputGloves = new String[]{"red", "red", "red", "red", "red"};
+        int expectedNumberPairGloves = 2;
+        PairOfGloves pairOfGloves = new PairOfGloves();
+
+        int numberPairGloves = pairOfGloves.getNumberPairGloves(inputGloves);
+
+        assertThat(numberPairGloves).isEqualTo(expectedNumberPairGloves);
+    }
 }
+
+
