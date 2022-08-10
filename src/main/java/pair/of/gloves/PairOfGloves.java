86cc7a12a14dc40208e66f33fe61a6057543bbb3
Natalia Hernandez <natalia.hernandez@clarity.ai>
2022-08-10 12:58:43 +0200

    Test if array is contains two gloves with the same color return 1 Pair of Gloves kata

--- src/main/java/pair/of/gloves/PairOfGloves.java
+++ src/main/java/pair/of/gloves/PairOfGloves.java
@@ -2,6 +2,10 @@ package pair.of.gloves;
 
 public class PairOfGloves {
     public int getNumberPairGloves(String[] inputGloves) {
+
+        if(inputGloves.length ==2 && inputGloves[0].equals(inputGloves[1])){
+            return 1;
+        }
         return 0;
     }
 }
--- src/test/java/pair/of/gloves/PairOfGlovesTest.java
+++ src/test/java/pair/of/gloves/PairOfGlovesTest.java
@@ -10,10 +10,33 @@ public class PairOfGlovesTest {
     void if_the_array_is_empty_return_0(){
         String[] inputGloves = new String[] {""};
         int expectedNumberPairGloves = 0;
-
         PairOfGloves pairOfGloves = new PairOfGloves();
+
         int numberPairGloves = pairOfGloves.getNumberPairGloves(inputGloves);
+
         assertThat(numberPairGloves).isEqualTo(expectedNumberPairGloves);
 
+    }
+
+    @Test
+    void if_the_array_contains_a_single_glove_return_0(){
+        String[] inputGloves = new String[] {"red"};
+        int expectedNumberPairGloves = 0;
+        PairOfGloves pairOfGloves = new PairOfGloves();
+
+        int numberPairGloves = pairOfGloves.getNumberPairGloves(inputGloves);
+
+        assertThat(numberPairGloves).isEqualTo(expectedNumberPairGloves);
+    }
+
+    @Test
+    void if_the_array_contains_two_gloves_with_the_same_color_return_1(){
+        String[] inputGloves = new String[]{"red", "red"};
+        int expectedNumberPairGloves = 1;
+        PairOfGloves pairOfGloves = new PairOfGloves();
+
+        int numberPairGloves = pairOfGloves.getNumberPairGloves(inputGloves);
+
+        assertThat(numberPairGloves).isEqualTo(expectedNumberPairGloves);
     }
 }
